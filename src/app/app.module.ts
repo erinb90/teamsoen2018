95828f994f35467ba01b69fddc4269af66b13133
Darrin Fong <darrinfong@gmail.com>
2018-10-30 20:09:26 -0400

    shittiest front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Withdrawal } from './withdrawal/withdrawal.component';
+import { routing } from './app.routes';
+import { Balance } from './balance/balance.component';
+import { Deposit } from './deposit/deposit.component';
+import { DepositCheck } from './deposit_check/deposit_check.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Balance,
+    Deposit,
+    DepositCheck,
+    Withdrawal
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
